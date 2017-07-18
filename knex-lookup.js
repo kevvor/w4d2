2d410a3ddaadf2b5051a955850b7af25b89e4ca7
keano voelkl <keanov@gmail.com>
2017-07-18 23:21:44 +0000

    can insert row from command line

--- knex-lookup.js
+++ knex-lookup.js
@@ -3,11 +3,17 @@ const name = process.argv[2];
 const settings = require('./settings');
 const knex = require('knex')(settings);
 
- knex.select().from('famous_people').where('first_name', 'like', '%'+name+'%').orWhere('first_name', 'like', '%'+name+'%').asCallback(function (err, result) {
-  console.log(`Found ${result.length} Person(s) with the name ${name}:`)
-  result.forEach(function (row,index){
-   let dob = row.birthdate.toString().substring(0,15);
-   console.log(`-${index+1} :${row.first_name} ${row.last_name}, born '${dob}`);
-  });
+knex.select()
+  .from('famous_people')
+  .where('first_name', 'like', '%'+name+'%')
+  .orWhere('first_name', 'like', '%'+name+'%')
+  .asCallback(function (err, result) {
+    console.log(`Found ${result.length} Person(s) with the name ${name}:`)
+    result.forEach(function (row,index){
+      let birthYear = row.birthdate.getUTCFullYear();
+      let birthMonth = row.birthdate.getUTCMonth();
+      let birthDate = row.birthdate.getUTCDate();
+     console.log(`-${index+1} :${row.first_name} ${row.last_name}, born '${birthYear}-${birthMonth}-${birthDate}'`);
+    });
   knex.destroy();
- })
+})
